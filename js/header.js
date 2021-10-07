fbdfcd0be7f4cf8eb92dfecfe80352a7ba1b78c2
Lennard <shparavalo78nenad@yahoo.com>
2021-10-07 12:20:23 +0200

    Create Gallery

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
-export const Romanian = {
-  title: "Școala de instruire a dealerilor de cazino",
-};
+// export const Romanian = {
+//   title: "Școala de instruire a dealerilor de cazino",
+// };
 
-export const English = {
-  title: "Casino Dealers Training School",
-};
+// export const English = {
+//   title: "Casino Dealers Training School",
+// };
 
-export const German = {
-  title: "Casino-Dealer-Ausbildungsschule",
-};
+// export const German = {
+//   title: "Casino-Dealer-Ausbildungsschule",
+// };
--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-import { Romanian, English, German } from "./header.js";
-/*
-  Change Language
-*/
-const roman = document.querySelector(".rom");
-const eng = document.querySelector(".eng");
-const ger = document.querySelector(".ger");
+// import { Romanian, English, German } from "./header.js";
+// /*
+//   Change Language
+// */
+// const roman = document.querySelector(".rom");
+// const eng = document.querySelector(".eng");
+// const ger = document.querySelector(".ger");
 
-function createHeader(language) {
-  return `
-    <h1>${language.title}</h1>
-    <h2>-Vlady Damas Group-</h2>
-    `;
-}
+// function createHeader(language) {
+//   return `
+//     <h1>${language.title}</h1>
+//     <h2>-Vlady Damas Group-</h2>
+//     `;
+// }
 
-const title = document.querySelector("header .main-text");
-const h1 = document.createElement("h1");
-h1.innerHTML = createHeader(English);
+// const title = document.querySelector("header .main-text");
+// const h1 = document.createElement("h1");
+// h1.innerHTML = createHeader(English);
 
-title.append(h1);
+// title.append(h1);
 
-roman.addEventListener("click", function () {
-  h1.innerHTML = createHeader(Romanian);
-});
-eng.addEventListener("click", function () {
-  h1.innerHTML = createHeader(English);
-});
-ger.addEventListener("click", function () {
-  h1.innerHTML = createHeader(German);
-});
+// roman.addEventListener("click", function () {
+//   h1.innerHTML = createHeader(Romanian);
+// });
+// eng.addEventListener("click", function () {
+//   h1.innerHTML = createHeader(English);
+// });
+// ger.addEventListener("click", function () {
+//   h1.innerHTML = createHeader(German);
+// });
