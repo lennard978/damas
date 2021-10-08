7da3d83f65b58c785a74c700e75b0aa7bbc0559f
Lennard <shparavalo78nenad@yahoo.com>
2021-10-08 12:10:24 +0200

    Update scroll function

--- js/main.js
+++ js/main.js
@@ -60,6 +60,7 @@ gsap
       // pin: true,
     },
   })
+  .from('.top', { opacity: 0 })
   .from('.top h3', { x: innerWidth * 1, opacity: 0 })
   .from('.top p', { x: innerWidth * -1, opacity: 0 })
   .from('.top li', { y: innerHeight * 1, opacity: 0, rotate: 360 })
@@ -75,8 +76,24 @@ gsap
       // pin: true,
     },
   })
+  .from('.bottom', { opacity: 0 })
   .from('.bottom h3', { x: innerWidth * 1, opacity: 0 })
   .from('.bottom p:nth-child(2)', { x: innerWidth * -1, opacity: 0 })
   .from('.bottom p:nth-child(3)', { x: innerWidth * 1, opacity: 0 })
   .from('.bottom p:nth-child(4)', { y: innerHeight * -1, opacity: 0 })
   .from('.bottom p:nth-child(5)', { y: innerHeight * 1, opacity: 0 })
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.banner',
+      start: '30% center',
+      end: '2% top',
+      // markers: true,
+      scrub: true,
+      // pin: true,
+    },
+  })
+  .from('.banner article:nth-child(1)', { opacity: 0, x: innerWidth * -1 })
+  .from('.banner article:nth-child(2)', { opacity: 0, x: innerWidth * 1 })
+  .from('.banner article:nth-child(3)', { opacity: 0, x: innerWidth * -1 })
