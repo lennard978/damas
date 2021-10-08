f091a002ad1cf48466389a7e9aa1943b5c82776a
Lennard <shparavalo78nenad@yahoo.com>
2021-10-08 13:54:56 +0200

    remove some js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,7 @@
-// import { Romanian, English, German } from "./header.js";
-// /*
-//   Change Language
-// */
-// const roman = document.querySelector(".rom");
-// const eng = document.querySelector(".eng");
-// const ger = document.querySelector(".ger");
-
-// function createHeader(language) {
-//   return `
-//     <h1>${language.title}</h1>
-//     <h2>-Vlady Damas Group-</h2>
-//     `;
-// }
-
-// const title = document.querySelector("header .main-text");
-// const h1 = document.createElement("h1");
-// h1.innerHTML = createHeader(English);
-
-// title.append(h1);
-
-// roman.addEventListener("click", function () {
-//   h1.innerHTML = createHeader(Romanian);
-// });
-// eng.addEventListener("click", function () {
-//   h1.innerHTML = createHeader(English);
-// });
-// ger.addEventListener("click", function () {
-//   h1.innerHTML = createHeader(German);
-// });
-
 let vh = window.innerHeight * 0.01
 document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 gsap.registerPlugin(ScrollTrigger)
-
-// gsap.from('.why', {
-//   x: -500,
-//   duration: 1,
-//   opacity: 0,
-//   scrollTrigger: {
-//     trigger: '.why',
-//     start: 'center center',
-//     end: 'bottom top',
-//     markers: true,
-//     //events: onEnter onLeave onEnterBack onLeaveBack
-//     toggleActions: 'restart complete reverse reset',
-//     //options: play, pause, resume, reset, restart, complete, reverse, none
-//   },
-// })
 
 gsap
   .timeline({
